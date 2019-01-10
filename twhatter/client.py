9539167374a67cc42a17c616c4d1e5102a4c1c71
theenglishway (time) <me@theenglishway.eu>
2019-01-10 19:32:08 +0100

    Use library for user-agent

--- twhatter/client.py
+++ twhatter/client.py
@@ -1,19 +1,13 @@
 import requests
-from random import choice
 from bs4 import BeautifulSoup
+from user_agent import generate_user_agent
 
 from twhatter.parser import TweetList, user_factory
 import json
 
 
 class Client():
-    HEADERS_LIST = [
-        'Mozilla/5.0 (Windows; U; Windows NT 6.1; x64; fr; rv:1.9.2.13) Gecko/20101203 Firebird/3.6.13',
-        'Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko',
-        'Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201',
-        'Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16',
-        'Mozilla/5.0 (Windows NT 5.2; RW; rv:7.0a1) Gecko/20091211 SeaMonkey/9.23a1pre'
-    ]
+    user_agent = generate_user_agent(os='linux')
 
     @classmethod
     def get_user_timeline(cls, user_handle):
@@ -21,7 +15,7 @@ class Client():
         return requests.get(
             url,
             headers={
-                'User-Agent': choice(cls.HEADERS_LIST),
+                'User-Agent': cls.user_agent,
                 'Accept-Language': 'en'
             }
         )
@@ -44,7 +38,7 @@ class ClientTimeline(Client):
                 max_position=self.earliest_tweet,
                 reset_error_state=False
             ),
-            headers={'User-Agent': choice(self.HEADERS_LIST)}
+            headers={'User-Agent': self.user_agent}
         )
 
     def __iter__(self):
